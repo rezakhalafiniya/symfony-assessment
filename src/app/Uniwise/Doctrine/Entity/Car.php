5d64d0c9a5291388b9f90c2a8348b90adbd8e5b3
Reza Khalafiniya <khalafiniya@fr-k.de>
2022-06-04 12:39:27 +0200

    Added properties

--- src/app/Uniwise/Doctrine/Entity/Car.php
+++ src/app/Uniwise/Doctrine/Entity/Car.php
@@ -21,4 +21,72 @@ class Car {
      * @var string
      */
     private $make;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $gasEconomy;
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getGasEconomy(): ?string
+    {
+        return $this->gasEconomy;
+    }
+
+    public function setGasEconomy(string $gasEconomy): self
+    {
+        $this->gasEconomy = $gasEconomy;
+
+        return $this;
+    }
 }
